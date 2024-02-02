e15776b438181a677a8a3354c0cf702042671415
josebrusa <josebrusa@outlook.com>
2024-02-02 15:49:22 +0100

    first commit

---
+++ src/stories/MyLabel.stories.ts
@@ -0,0 +1,54 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { MyLabel } from "../component/MyLabel";
+
+
+
+
+const meta = {
+    title: 'UI/labels/MyLabel',
+    component: MyLabel, 
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'centered'
+    },
+    argTypes: {
+        size: { control: 'inline-radio' },
+        color: { control: 'select' },
+        fontColor: { control: 'color' } ,
+    }
+} satisfies Meta<typeof MyLabel>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>
+
+
+export const Basic: Story = {
+    args: {
+        label: 'Basic label',
+    }
+}
+export const AllCaps: Story = {
+    args: {
+        label: 'All Caps label',
+        allCaps: true,
+    }
+}
+export const Secondary: Story = {
+    args: {
+        label: 'Secondary label',
+        color: 'text-primary',
+    }
+}
+export const CustomColor: Story = {
+    args: {
+        label: 'Custom Color label',
+        fontColor: '#5517ac',
+    }
+}
+
+
+
+
+
+
